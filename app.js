a3231dfe9d209eed604374f31d796fd0d62a34be
Rajeev Ranjan <rajeev-ra@HCL.COM>
2017-12-08 14:19:46 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+// Listen for messages from users
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, [
+    // session.send("You said: %s", session.message.text);
+    function (session) {
+        session.beginDialog('greeting');
+    }
+]);
+
+bot.dialog('greetings', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.endDialog(`Hello ${results.response}!`);
+    }
+]);
+
+
+bot.dialog('greeting', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! Whats ur name?');
+    },
+    function (session, results) {
+        session.endDialog(`Hello ${results.response}!`);
+    }
+]);
